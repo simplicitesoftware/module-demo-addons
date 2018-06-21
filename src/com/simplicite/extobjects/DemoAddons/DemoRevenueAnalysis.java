c2db31d738aaf5c9fbc714705f0c8b8e4134bd9b
David AZOULAY <dazoulay@simplicite.fr>
2018-06-21 12:16:07 +0200

    Removed warnings

--- src/com/simplicite/extobjects/DemoAddons/DemoRevenueAnalysis.java
+++ src/com/simplicite/extobjects/DemoAddons/DemoRevenueAnalysis.java
@@ -1,16 +1,19 @@
 package com.simplicite.extobjects.DemoAddons;
 
-import java.util.*;
-import com.simplicite.util.*;
-import com.simplicite.util.tools.*;
-import org.json.*;
+import com.simplicite.util.AppLog;
+import com.simplicite.util.Crosstab;
+import com.simplicite.util.ExternalObject;
+import com.simplicite.util.ObjectDB;
+import com.simplicite.util.Tool;
+import com.simplicite.util.tools.HTMLTool;
+import com.simplicite.util.tools.Parameters;
 
 /**
  * External object ExampleDevExtreme
  */
 public class DemoRevenueAnalysis extends ExternalObject {
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * Display method
 	 * @param params Request parameters
@@ -27,7 +30,7 @@ public class DemoRevenueAnalysis extends ExternalObject {
 				"https://cdn3.devexpress.com/jslib/18.1.3/js/dx.all.js",
 				getResourceJSURL("SCRIPT")
 			});
-			
+
 			String inst = params.getParameter("inst");
 			ObjectDB obj = getGrant().getObject(inst, "DemoOrder");
 			String fields = "[" +
@@ -42,18 +45,18 @@ public class DemoRevenueAnalysis extends ExternalObject {
 			c.build(obj);
 			String data = c.toJSONCubes();
 			c.clear();
-			
+
 			return
 				"<div class=\"dx-viewport demo-container\">" +
-        			"<div id=\"pivotgrid-demo\">" +
-        				"<div id=\"pivotgrid-chart\"></div>" +
-        				"<div id=\"pivotgrid\">" + getGrant().T("LOADING") + "</div>" +
-        			"</div>" +
-    			"</div>" +
-    			HTMLTool.jsBlock(
-    				"onload_functions.push(function() {" +
-    					"grid(" + fields + ", " + data + ", null, "+ Tool.getCurrentYear() +");" +
-    				"});");
+					"<div id=\"pivotgrid-demo\">" +
+						"<div id=\"pivotgrid-chart\"></div>" +
+						"<div id=\"pivotgrid\">" + getGrant().T("LOADING") + "</div>" +
+					"</div>" +
+				"</div>" +
+				HTMLTool.jsBlock(
+					"onload_functions.push(function() {" +
+						"grid(" + fields + ", " + data + ", null, "+ Tool.getCurrentYear() +");" +
+					"});");
 		} catch (Exception e) {
 			AppLog.error(getClass(), "display", null, e, getGrant());
 			return e.getMessage();

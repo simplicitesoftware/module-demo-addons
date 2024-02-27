37d88a300ce6bf8cce5a2dd74b032a8fc5791879
designer (Designer) <simplicite@simplicite.fr>
2024-02-27 12:01:09 +0100

    Modernized JS syntax

--- resources/ObjectExternal/DemoRevenueAnalysis/SCRIPT.js
+++ resources/ObjectExternal/DemoRevenueAnalysis/SCRIPT.js
@@ -2,64 +2,62 @@
 // Client side JavaScript for revenue analysis
 //-----------------------------------------------------------
 
-var DemoRevenueAnalysis = DemoRevenueAnalysis || (function() {
+class DemoRevenueAnalysis {
 
-function render(fields, data, expRow, expColumn, dateFormat) {
-	DevExpress.viz.currentTheme("generic.light");
-
-	// ZZZ convert localized date fields' strings as dates
-	for (const f of fields)
-		if (f.dataType === "date")
-			for (const d of data)
-				d[f.dataField] = moment(d[f.dataField], dateFormat).toDate();
+	static render(fields, data, expRow, expColumn, dateFormat) {
+		DevExpress.viz.currentTheme("generic.light");
 	
-	const pivotGridChart = $("#DemoRevenueAnalysis-chart").dxChart({
-		commonSeriesSettings: { type: "bar" },
-		tooltip: {
-			enabled: true,
-			format: "currency",
-			customizeTooltip: function(args) {
-				return {
-					html: args.seriesName + " | Total<div class='currency'>" + args.valueText + "</div>"
-				};
+		// ZZZ convert localized date fields' strings as dates
+		for (const f of fields)
+			if (f.dataType === "date")
+				for (const d of data)
+					d[f.dataField] = moment(d[f.dataField], dateFormat).toDate();
+		
+		const pivotGridChart = $("#DemoRevenueAnalysis-chart").dxChart({
+			commonSeriesSettings: { type: "bar" },
+			tooltip: {
+				enabled: true,
+				format: "currency",
+				customizeTooltip: function(args) {
+					return {
+						html: args.seriesName + " | Total<div class='currency'>" + args.valueText + "</div>"
+					};
+				}
+			},
+			size: { height: 300 },
+			adaptiveLayout: { width: 450 }
+		}).dxChart("instance");
+	
+		const pivotGrid = $("#DemoRevenueAnalysis-grid").dxPivotGrid({
+			allowSortingBySummary: true,
+			allowFiltering: true,
+			showBorders: true,
+			showColumnGrandTotals: true,
+			showRowGrandTotals: true,
+			showRowTotals: false,
+			showColumnTotals: false,
+			fieldChooser: {
+				enabled: true,
+				height: 400
+			},
+			dataSource: {
+				fields: fields,
+				store: data
 			}
-		},
-		size: { height: 300 },
-		adaptiveLayout: { width: 450 }
-	}).dxChart("instance");
-
-	const pivotGrid = $("#DemoRevenueAnalysis-grid").dxPivotGrid({
-		allowSortingBySummary: true,
-		allowFiltering: true,
-		showBorders: true,
-		showColumnGrandTotals: true,
-		showRowGrandTotals: true,
-		showRowTotals: false,
-		showColumnTotals: false,
-		fieldChooser: {
-			enabled: true,
-			height: 400
-		},
-		dataSource: {
-			fields: fields,
-			store: data
-		}
-	}).dxPivotGrid("instance");
-
-	pivotGrid.bindChart(pivotGridChart, {
-		dataFieldsDisplayMode: "splitPanes",
-		alternateDataFields: false
-	});
-
-	function expand() {
-		const dataSource = pivotGrid.getDataSource();
-		if (expRow) dataSource.expandHeaderItem("row", [expRow]);
-		if (expColumn) dataSource.expandHeaderItem("column", [expColumn]);
+		}).dxPivotGrid("instance");
+	
+		pivotGrid.bindChart(pivotGridChart, {
+			dataFieldsDisplayMode: "splitPanes",
+			alternateDataFields: false
+		});
+	
+		const expand = () => {
+			const dataSource = pivotGrid.getDataSource();
+			if (expRow) dataSource.expandHeaderItem("row", [expRow]);
+			if (expColumn) dataSource.expandHeaderItem("column", [expColumn]);
+		};
+	
+		setTimeout(expand, 0);
 	}
 
-	setTimeout(expand, 0);
 }
-
-return { render: render };
-
-})();
